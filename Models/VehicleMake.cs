84467a16bf7f39db13435ce99a4b494d9e2a8ed8
matejsarlija <matejsarlija1@gmail.com>
2023-01-02 09:52:43 +0100

    models ef

--- Models/VehicleMake.cs
+++ Models/VehicleMake.cs
@@ -2,5 +2,9 @@ namespace Vehicles.Models;
 
 public class VehicleMake
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Abrv { get; set; }
+
+    public ICollection<VehicleModel> VehicleModels { get; set; }
 }
--- Models/VehicleModel.cs
+++ Models/VehicleModel.cs
@@ -2,5 +2,10 @@ namespace Vehicles.Models;
 
 public class VehicleModel
 {
-    
+     public int Id { get; set; }
+     public string Name { get; set; }
+     public string Abrv { get; set; }
+
+     public VehicleMake VehicleMake { get; set; }
+
 }
