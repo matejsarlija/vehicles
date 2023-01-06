490186f7a45a4b75c60d1fe0e919d3bc09ee09c2
matejsarlija <matejsarlija1@gmail.com>
2023-01-06 20:15:01 +0100

    hasPreviousPage instead of HasPreviousPage, much plumbing for a typo

--- Models/VehicleModelViewModel.cs
+++ Models/VehicleModelViewModel.cs
@@ -4,7 +4,7 @@ namespace Vehicles.Models;
 
 public class VehicleModelViewModel
 {
-    public List<VehicleModel> VehicleModels { get; set; }
+    public PaginatedList<VehicleModel> VehicleModels { get; set; }
     public SelectList VehicleMakes { get; set; }
     public string VehicleModelMake { get; set; }
     public string SearchString { get; set; }
